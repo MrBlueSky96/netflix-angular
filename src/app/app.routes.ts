8bbbcb1fcd7b900128278caacb2e15c1a2781e7c
Jose.G <josegonzalezguilabert@gmail.com>
2026-03-26 11:21:40 +0100

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { SeriesComponent } from './components/series/series.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'peliculas', component: PeliculasComponent },
-  { path: 'series', component: SeriesComponent },
-  { path: '', redirectTo: 'peliculas', pathMatch: 'full' }
+  { path: 'series', component: SeriesComponent }
 ];
