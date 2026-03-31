9295693d78ccbb18e79ab6f6f9ff4d74b9044832
Jose.G <josegonzalezguilabert@gmail.com>
2026-03-31 13:40:54 +0200

    sistema registro y login

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule ],
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  constructor(public authService: AuthService) {}
+
+  getUsername(): string | null {
+    return localStorage.getItem('username');
+  }
+
+  logout() {
+    this.authService.logout();
+    window.location.href = '/login'; // o router.navigate
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  platformId = inject(PLATFORM_ID);
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any) {
+    return this.http.post<any>(`${this.apiUrl}/login`, credentials)
+      .pipe(
+        tap(response => {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('username', response.username);
+        })
+      );
+  }
+
+  register(user: any) {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+  }
+
+  getToken(): string | null {
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem('token');
+    }
+    return null;
+  }
+
+  isLogged(): boolean {
+    return !!this.getToken();
+  }
+}
